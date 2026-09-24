99b5e202e5977efaacd84c8d58aaea5b88797d24
CharbelJ24 <charbeljanosfreitas@gmail.com>
2026-09-24 00:36:59 +0000

    inicia a1

--- a1.c
+++ a1.c
@@ -5,10 +5,22 @@
 int main()
 {
     int PC, N;
-    PC = 0;
+    PC = 1;
 
     while(PC < MAX){
         PC++;
+        sleep(0.5);
+        if (d = rand()%100 + 1 < 15){
+            if(d % 2){
+                Op = R;
+                Data = &N;
+            }
+            else{
+                Op = W;
+                Data = &PC;
+            }
+        }
+        sleep(0.5);
     }
 
     return 0;
